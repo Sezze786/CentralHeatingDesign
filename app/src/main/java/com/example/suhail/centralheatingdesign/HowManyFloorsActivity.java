ae9e7a7b9b658d356ddf922f132c188721352301
Suhail <sezze786@gmail.com>
2017-02-24 13:13:37 +0000

    Created Overview Activity and sorted a few buttons out

--- app/src/main/java/com/example/suhail/centralheatingdesign/HowManyFloorsActivity.java
+++ app/src/main/java/com/example/suhail/centralheatingdesign/HowManyFloorsActivity.java
@@ -1,5 +1,6 @@
 package com.example.suhail.centralheatingdesign;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,7 +27,7 @@ public class HowManyFloorsActivity extends AppCompatActivity implements AdapterV
         numberOfFloorsSpinner.setOnItemSelectedListener(this);
 
         // Let 'back' button open the main activity
-        Button loadMainActivity = (Button) findViewById(R.id.backButton);
+        Button loadMainActivity = (Button) findViewById(R.id.mainButton);
         loadMainActivity.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -35,40 +36,58 @@ public class HowManyFloorsActivity extends AppCompatActivity implements AdapterV
                 startActivity(intent);
             } // onClick
         });
+
+        // Let 'next' button open the overview activity
+        Button loadOverviewActivity = (Button) findViewById(R.id.overviewButton);
+        loadOverviewActivity.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HowManyFloorsActivity.this, OverviewActivity.class);
+                startActivity(intent);
+            } // onClick
+        });
     } // onCreate
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 //        textView.setText(numberOfFloorsSpinner.getSelectedItem().toString());
         float weight = (float) 0.5;
 
-//        // Delete all layouts except the Spinner and text box layout
-//        linearLayout.removeAllViewsInLayout();
-        for(int i = 1; i <= 4; i++)
-        {
-            LinearLayout ll = (LinearLayout) findViewById(i);
+        // Delete all layouts except the Spinner and text box layout
+        removeDynamicLayouts();
 
-
-            if(ll != null)
-            {
-                linearLayout.removeView(ll);
-            }
-        }
         // How many floors do we want
         numberOfFloors = Integer.parseInt(numberOfFloorsSpinner.getSelectedItem().toString());
-        // Create x amount of layouts
+        // Create i amount of layouts
         for (int i = 1; i <= numberOfFloors; i++) {
             LinearLayout ll = new LinearLayout(this);
             ll.setOrientation(LinearLayout.VERTICAL);
             ll.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, weight));
             ll.setId(i);
             linearLayout.addView(ll);
+            // Add Floor i text
             TextView tx1 = new TextView(this);
-            tx1.setText(Integer.toString(i));
+            tx1.setText("Floor " + Integer.toString(i));
             ll.addView(tx1);
+            // Add 'add a room' button
+            Button addRoom = new Button(this);
+            ll.addView(addRoom);
+            addRoom.setText("Add Room");
         } // for
     } // onItemSelected
 
+
+
+    private void removeDynamicLayouts(){
+        for(int i = 1; i <= 4; i++)
+        {
+            LinearLayout ll = (LinearLayout) findViewById(i);
+            if(ll != null)
+                linearLayout.removeView(ll);
+        }
+    } // removeDynamicLayouts
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
